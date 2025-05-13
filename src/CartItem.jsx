b4ba6cd7ec5e8d5ed3c20bced111ce06a7a05561
ibekwezillio <ibekwezillion@gmail.com>
2025-05-13 07:59:07 -0400

    Fix calculateTotalAmount function and build error

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,14 +8,14 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  function calculateTotalAmount(cart) {
+  const calculateTotalAmount = () => {
     let total = 0;
     cart.forEach(item => {
-        const cost = parseFloat(item.cost.substring(1));
-        total += item.quantity * cost;
+      const cost = parseFloat(item.cost.replace(/[^0-9.-]+/g, ''));
+      total += item.quantity * cost;
     });
-    return total;
-}
+    return total.toFixed(2);
+  };
 
   cart.forEach(item => {
     const quantity = item.quantity;
@@ -50,7 +50,7 @@ const CartItem = ({ onContinueShopping }) => {
   
 
   const handleRemove = (item) => {
-        dispatch(removeItem(itemName));
+        dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
@@ -70,24 +70,44 @@ const CartItem = ({ onContinueShopping }) => {
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button 
+                  className="cart-item-button cart-item-button-dec" 
+                  onClick={() => handleDecrement(item)}
+                >
+                  -
+                </button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button 
+                  className="cart-item-button cart-item-button-inc" 
+                  onClick={() => handleIncrement(item)}
+                >
+                  +
+                </button>
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <button 
+                className="cart-item-delete" 
+                onClick={() => handleRemove(item)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         ))}
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button 
+          className="get-started-button" 
+          onClick={handleContinueShopping}
+        >
+          Continue Shopping
+        </button>
         <br />
         <button className="get-started-button1">Checkout</button>
       </div>
     </div>
-  );
+);
 
 export default CartItem;
 
